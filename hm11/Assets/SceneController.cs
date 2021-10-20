9cebca2bbedb4499b1bce87a368a4b2b21f20fe4
PastryChew <80216822+PastryChew@users.noreply.github.com>
2021-10-20 15:15:42 +0300

    11

---
+++ hm11/Assets/SceneController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneController : MonoBehaviour
+{
+   
+    public void LoadScene(int numberlvl)
+    {
+        
+        SceneManager.LoadScene(numberlvl);
+    }
+
+}
